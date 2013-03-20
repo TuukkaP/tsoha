68325e05622782157f9454d6caf2a3a320d29903
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-03-20 19:46:09 +0200

    PHP:n alku eli juuri

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <?php
+        include ('login.php');
+        ?>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+include('lock.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Welcome <?php echo $login_session; ?></h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
