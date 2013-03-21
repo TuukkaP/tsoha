260f91e5d61aa82b63661412c2ca4b3cf974775b
Ilkka Vähämaa <ilkka.vahamaa@helsinki.fi>
2013-03-21 14:14:09 +0200

    test on dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("pgsql:host=localhost;dbname=ivahamaa",
-                      "ivahamaa", "49fbd656411c2b0d");
+    $pdo = new PDO('pgsql:host=localhost;dbname=ivahamaa','ivahamaa', '49fbd656411c2b0d');
 } catch (PDOException $e) {
     die("VIRHE: " . $e->getMessage());
 }
