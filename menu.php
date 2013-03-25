9cd2592422ce5c682a36ed3de07ba0dcb43a687d
Ilkka Vähämaa <ilkka.vahamaa@helsinki.fi>
2013-03-25 13:49:50 +0200

    Päivitetty menua ja kyselyitä

--- menu.php
+++ menu.php
@@ -5,10 +5,19 @@ include ('lock.php');
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title> Company ab - Workscheduling</title>
     </head>
     <body>
         <h1>Welcome <?php echo $session->username; ?></h1>
         <a href="logout.php">Logout</a>
+
+        <h1>Ominaisuuksia to be built</h1>
+
+        <ul>
+            <li>Listaa vapaat työvuorot</li>
+            <li>Listaa omat työvuorot</li>
+            <li>ADMIN: Lisää työvuoro</li>
+            <li>ADMIN: Poista työvuoro</li>
+        </ul>
     </body>
 </html>
--- tools/queries.php
+++ tools/queries.php
@@ -2,21 +2,27 @@
 
 class Queries {
 
-  private $_pdo;
+    private $_pdo;
 
-  public function __construct($pdo) {
-    $this->_pdo = $pdo;
-  }
-  
-    public function login($username, $password) {
-    $sql = $this->_pdo->prepare('SELECT username FROM users WHERE username = ? AND password = ?');
-    if ($sql->execute(array($username, $password))) {
-      return $sql->fetchObject();
-    } else {
-      return null;
+    public function __construct($pdo) {
+        $this->_pdo = $pdo;
     }
-  }
+
+    public function login($username, $password) {
+        $sql = $this->_pdo->prepare('SELECT username FROM users WHERE username = ? AND password = ?');
+        if ($sql->execute(array($username, $password))) {
+            return $sql->fetchObject();
+        } else {
+            return null;
+        }
+    }
+
+    public function listAllAvailableOrders() {
+        $sql = $this->_pdo->prepare('SELECT id,place,orders.date,start,stop FROM orders WHERE employee IS NULL ORDER BY date');
+        return $sql->fetchObject();
+    }
 }
-require dirname(__file__).'/../dbconnect.php';
+
+require dirname(__file__) . '/../dbconnect.php';
 $queries = new Queries($pdo);
 
