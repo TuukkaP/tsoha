4d9bafa5c24cb0cf40070d3768ebad7e1244408c
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-04-01 19:03:00 +0300

    Rooli dropdown menu

--- correctUsers.php
+++ correctUsers.php
@@ -16,12 +16,16 @@ include ('lock.php');
     <tbody>
         <tr><form action="update.php" method="POST"><input type = "hidden" name = "table" value = "users"/>
         <?php
-        echo '<td><input type = "text" name = "username" value = "' . $_POST['username'] . '"/></td>';
-        echo '<td><input type = "text" name = "firstname" value = "' . $_POST['firstname'] . '"/></td>';
-        echo '<td><input type = "text" name = "lastname" value = "' . $_POST['lastname'] . '"/></td>';
-        echo '<td><input type = "text" name = "address" value = "' . $_POST['address'] . '"/></td>';
-        echo '<td><input type = "text" name = "email" value = "' . $_POST['email'] . '"/></td>';
-        echo '<td><input type = "text" name = "role" value = "' . $_POST['role'] . '"/></td>';
+        echo "\t<td><input type = \"text\" name = \"username\" value = \"" . $_POST['username'] . "\"/></td>\n";
+        echo "\t<td><input type = \"text\" name = \"firstname\" value = \"" . $_POST['firstname'] . "\"/></td>";
+        echo "\t<td><input type = \"text\" name = \"lastname\" value = \"" . $_POST['lastname'] . "\"/></td>";
+        echo "\t<td><input type = \"text\" name = \"address\" value =\"" . $_POST['address'] . "\"/></td>";
+        echo "\t<td><input type = \"text\" name = \"email\" value = \"" . $_POST['email'] . "\"/></td>";
+        echo "\t<td><select name=\"role\" value=\"options\">
+              \t\t  <option selected=\"selected\">" . $_POST['role'] . "</option>
+              \t\t  <option value=\"admin\">Admin</option>
+              \t\t <option value=\"user\">User</option>
+            \t</select></td>";
         ?>
         <td> <input type="submit" value="Korjaa" name="korjaa"/><br>
         <input type="submit" value="Poista" name="poista"/></form></td>
--- newUser.php
+++ newUser.php
@@ -1,22 +1,15 @@
 <?php
 include ('lock.php');
-if(isset($_POST['add'])) {
+if (isset($_POST['add'])) {
     $add = $queries->addUser(
-            filter_var($_POST['username'], FILTER_SANITIZE_STRING),
-            hash("sha256", filter_var($_POST['password'], FILTER_SANITIZE_STRING).filter_var($_POST['username'], FILTER_SANITIZE_STRING)),
-            filter_var($_POST['firstname'], FILTER_SANITIZE_STRING), 
-            filter_var($_POST['lastname'], FILTER_SANITIZE_STRING), 
-            filter_var($_POST['address'], FILTER_SANITIZE_STRING), 
-            filter_var($_POST['email'], FILTER_SANITIZE_EMAIL), 
-            filter_var($_POST['role'], FILTER_SANITIZE_STRING)
-            );
-    if($add == true) {
+            filter_var($_POST['username'], FILTER_SANITIZE_STRING), hash("sha256", filter_var($_POST['password'], FILTER_SANITIZE_STRING) . filter_var($_POST['username'], FILTER_SANITIZE_STRING)), filter_var($_POST['firstname'], FILTER_SANITIZE_STRING), filter_var($_POST['lastname'], FILTER_SANITIZE_STRING), filter_var($_POST['address'], FILTER_SANITIZE_STRING), filter_var($_POST['email'], FILTER_SANITIZE_EMAIL), filter_var($_POST['role'], FILTER_SANITIZE_STRING)
+    );
+    if ($add == true) {
         header("location: listUsers.php");
     } else {
         header("location: menu.php");
     }
 }
-
 ?>
 
 <table border = "1">
@@ -33,15 +26,17 @@ if(isset($_POST['add'])) {
     </thead>
     <tbody>
         <tr><form action="" method="POST">
-        <?php
-        echo '<td><input type = "text" name = "username" value = ""/></td>';
-        echo '<td><input type = "text" name = "password" value = ""/></td>';
-        echo '<td><input type = "text" name = "firstname" value = ""/></td>';
-        echo '<td><input type = "text" name = "lastname" value = ""/></td>';
-        echo '<td><input type = "text" name = "address" value = ""/></td>';
-        echo '<td><input type = "text" name = "email" value = ""/></td>';
-        echo '<td><input type = "text" name = "role" value = ""/></td>';
-        ?>
+        <td><input type = "text" name = "username" value = ""/></td>
+        <td><input type = "text" name = "password" value = ""/></td>
+        <td><input type = "text" name = "firstname" value = ""/></td>
+        <td><input type = "text" name = "lastname" value = ""/></td>
+        <td><input type = "text" name = "address" value = ""/></td>
+        <td><input type = "text" name = "email" value = ""/></td>
+        <td><select name="role" value="options">
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+            </select>
+        </td>
         <td><input type="submit" value="Lisää" name="add"/></form></td>
 </tr>
 </tbody>
