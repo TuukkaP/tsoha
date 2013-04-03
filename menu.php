6940550202e81c3b035e4a62001557e6f91039d3
Ilkka Vähämaa <ilkka.vahamaa@helsinki.fi>
2013-04-03 11:29:06 +0300

    päivitetty menuun toppi

--- menu.php
+++ menu.php
@@ -1,12 +1,10 @@
 <?php
-include ('lock.php');
+include ('top.php');
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> Company ab - Workscheduling</title>
-    </head>
+
     <body>
         <h1>Welcome <?php echo $session->username; ?></h1><br>
         
--- tools/top.php
+++ tools/top.php
@@ -1,7 +1,20 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+include ('lock.php');
 ?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title> Company ab - Workscheduling</title>
+        <link rel="styles" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <p style="text-align:right;">
+            <?php  
+            echo date('l, F jS, Y');
+            echo $session->username;           
+            ?>
+            <a href="logout.php">Logout</a>;
+        </p>        
+    </body>    
+</html>
+
